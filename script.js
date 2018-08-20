d9bb38a77da0b7030ffa6fc600463de07f490748
guilherme <gui-keiroz@live.com>
2018-08-20 10:54:35 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+let nome = prompt("Digite aqui o seu nome")
+
+console.log(`O nome digitado foi ${nome}`);
+
+alert(`Seja bem vindo, ${nome}! Aproveite sua visita `);
+
+let numero = prompt("Digite aqui o numero");    
+
+if(numero >10){
+    console.log("É um numero maior que 10");
+}
+
+else{
+    console.log ("Não é um numero maior que 10")
+}
+
+else{
+    console.log("heahu");
+}
+
+if(numero > 10){
+    console.log ("É um numero maior que 10")
+}
+
+if(numero <= 10){
+    console.log("Não é um numero maior que 10");
+}
